caa604d750dc806727736ea91389a5ad96f0f6fb
farihyuti <79531054+farihyuti@users.noreply.github.com>
2021-03-03 11:16:17 +0700

    Update image_reverse_search.py

--- SaitamaRobot/modules/image_reverse_search.py
+++ SaitamaRobot/modules/image_reverse_search.py
@@ -184,7 +184,7 @@ __help__ = f"""
 
 • `/reverse`*:* Does a *reverse image search* of the media which it was replied to.
 
-report bugs in @hikigayasupport
+report bugs in @mihaiflorecubot
 """
 
 __mod_name__ = "Image Search"
