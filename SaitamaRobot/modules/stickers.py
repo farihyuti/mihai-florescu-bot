a4cb4babc782f675c605a72956e34e90c68222f2
Clara <79531054+farihyuti@users.noreply.github.com>
2021-06-03 10:07:01 +0700

    Update stickers.py

--- SaitamaRobot/modules/stickers.py
+++ SaitamaRobot/modules/stickers.py
@@ -1,6 +1,7 @@
 import os
 import math
 import requests
+import cloudscraper
 import urllib.request as urllib
 from PIL import Image
 from html import escape
@@ -37,23 +38,24 @@ def stickerid(update: Update, context: CallbackContext):
         )
 
 
-@run_async
 def cb_sticker(update: Update, context: CallbackContext):
     msg = update.effective_message
-    split = msg.text.split(' ', 1)
+    split = msg.text.split(" ", 1)
     if len(split) == 1:
-        msg.reply_text('Provide some name to search for pack.')
+        msg.reply_text("Provide some name to search for pack.")
         return
-    text = requests.get(combot_stickers_url + split[1]).text
-    soup = bs(text, 'lxml')
-    results = soup.find_all("a", {'class': "sticker-pack__btn"})
+
+    scraper = cloudscraper.create_scraper()
+    text = scraper.get(combot_stickers_url + split[1]).text
+    soup = bs(text, "lxml")
+    results = soup.find_all("a", {"class": "sticker-pack__btn"})
     titles = soup.find_all("div", "sticker-pack__title")
     if not results:
-        msg.reply_text('No results found :(.')
+        msg.reply_text("Not found so sorry :(.")
         return
-    reply = f"Stickers for *{split[1]}*:"
+    reply = f"Found stickers for *{split[1]}*:"
     for result, title in zip(results, titles):
-        link = result['href']
+        link = result["href"]
         reply += f"\n• [{title.get_text()}]({link})"
     msg.reply_text(reply, parse_mode=ParseMode.MARKDOWN)
 
